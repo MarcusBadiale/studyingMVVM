d5e67bad202f5121541d282246f550c148645934
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2020-12-01 15:31:25 -0300

    Finishing web service.

--- CoffeOrdering/CoffeOrdering/Services/WebService.swift
+++ CoffeOrdering/CoffeOrdering/Services/WebService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum NetworkError: Error {
-    case decodingeRROR
+    case decodingError
     case domainError
     case urlError
 }
@@ -21,7 +21,24 @@ class WebService {
     
     func load<T>(resource: Resource<T>, completion: @escaping (Result<T, NetworkError>) -> Void) {
         
-    
+        URLSession.shared.dataTask(with: resource.url) { (data, response, error) in
+            
+            guard let data = data, error == nil else {
+                completion(.failure(.domainError))
+                return
+            }
+            
+            let result = try? JSONDecoder().decode(T.self, from: data)
+            
+            if let result = result {
+                DispatchQueue.main.async {
+                    completion(.success(result))
+                }
+            } else {
+                completion(.failure(.decodingError))
+            }
+            
+        }.resume()
         
     }
 }
