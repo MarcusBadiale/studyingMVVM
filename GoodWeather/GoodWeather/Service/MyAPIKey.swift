cb41b9a16ccdb98f581808d6d97e7ac57a905209
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2020-12-04 12:39:44 -0300

    Adding my api key

---
+++ GoodWeather/GoodWeather/Service/MyAPIKey.swift
@@ -0,0 +1,13 @@
+//
+//  MyAPIKey.swift
+//  GoodWeather
+//
+//  Created by Marcus Vinicius Vieira Badiale on 04/12/20.
+//
+
+import Foundation
+
+struct MyAPIKey {
+    
+    static let myApiKey = "a9caa960d3867deda702fd9fc8ca34f0"
+}
